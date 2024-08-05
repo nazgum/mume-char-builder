a1d2c07f345aea9d5a54e02c5f61db78fa9282ce
Nazgum <work@nazgum.com>
2024-08-04 20:04:37 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.set('view engine', 'ejs')
+
+app.use(express.static('static'))
+
+app.get('/', (req, res) => {
+  res.render('pages/index')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
