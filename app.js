0e31b826a20bcdc19e33001d8b021dc4b1766308
Nazgum <work@nazgum.com>
2024-08-12 14:22:15 -0400

    enable simple server.log

--- app.js
+++ app.js
@@ -2,14 +2,28 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const fs = require('fs');
+
 app.set('view engine', 'ejs')
 
 app.use(express.static('static'))
 
 // logger
 app.use((req,res,next) =>{
-  req.time = new Date(Date.now()).toString();
-  console.log(req.method,req.hostname, req.path, req.time);
+  const timestamp = new Date().toLocaleString('en-US', {
+    month: 'short',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: false
+  }).replace(',', '');
+
+  const logMessage = `${timestamp}   ${req.method}   ${req.ip}   ${req.path}   ${res.statusCode}\n`;
+
+  // Log to a file if needed
+  fs.appendFileSync('server.log', logMessage);
+
   next();
 });
 
